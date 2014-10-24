81b4462a614aa809ee6c3b260ff4e1c293703370
Lauri Raivio <lauri.raivio@gmail.com>
2014-10-24 09:26:27 +0300

    Add column tests to hearing

--- tests/factories.py
+++ tests/factories.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import factory
+
 from kuulemma.models import Hearing
 from tests.sqlalchemy_model_factory import SQLAlchemyModelFactory
 
 
 class HearingFactory(SQLAlchemyModelFactory):
     FACTORY_FOR = Hearing
-    title = 'Important Hearing'
+    title = factory.Sequence(lambda n: u'Hearing {0}'.format(n))
--- tests/models/test_hearing.py
+++ tests/models/test_hearing.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import pytest
+from sqlalchemy_utils import (
+    assert_max_length,
+    assert_non_nullable,
+    assert_nullable
+)
+
 from ..factories import HearingFactory
 
 
@@ -7,3 +14,28 @@ class TestHearing(object):
         hearing = HearingFactory.build()
         expected = '<Hearing title=\'{title}\'>'.format(title=hearing.title)
         assert repr(hearing) == expected
+
+
+@pytest.mark.usefixtures('database')
+class TestHearingWithDatabase(object):
+    @pytest.fixture(scope='function')
+    def hearing(self):
+        return HearingFactory()
+
+    def test_created_at_is_non_nullable(self, hearing):
+        assert_non_nullable(hearing, 'created_at')
+
+    def test_updated_at_is_nullable(self, hearing):
+        assert_nullable(hearing, 'updated_at')
+
+    def test_title_is_non_nullable(self, hearing):
+        assert_non_nullable(hearing, 'title')
+
+    def test_title_max_length_is_255(self, hearing):
+        assert_max_length(hearing, 'title', 255)
+
+    def test_lead_is_non_nullable(self, hearing):
+        assert_non_nullable(hearing, 'lead')
+
+    def test_body_is_non_nullable(self, hearing):
+        assert_non_nullable(hearing, 'body')
