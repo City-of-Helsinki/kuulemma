e10fa53439b622f4ec586d9ac289da1dbd5a3637
Lauri Raivio <lauri.raivio@gmail.com>
2014-11-02 13:39:32 +0200

    Add API for liking comments

--- kuulemma/views/comment.py
+++ kuulemma/views/comment.py
@@ -1,24 +1,26 @@
-from flask import Blueprint, jsonify, request
+from flask import abort, Blueprint, jsonify, request
+from flask.ext.login import current_user
+from sqlalchemy.orm.exc import NoResultFound
 
 from kuulemma.extensions import db
-from kuulemma.models import Comment, Hearing
+from kuulemma.models import Comment, Hearing, Like, User
 from kuulemma.schemas import CommentSchema
 
 comment = Blueprint(
     name='comment',
     import_name=__name__,
-    url_prefix='/kuulemiset/<int:hearing_id>/links/comments'
+    url_prefix=''
 )
 
 
-@comment.route('')
+@comment.route('/kuulemiset/<int:hearing_id>/links/comments')
 def index(hearing_id):
     comments = Comment.query.filter(Comment.hearing_id == hearing_id).all()
     serialized = CommentSchema(comments, many=True)
     return jsonify({'comments': serialized.data}), 200
 
 
-@comment.route('', methods=['POST'])
+@comment.route('/kuulemiset/<int:hearing_id>/links/comments', methods=['POST'])
 def create(hearing_id):
     hearing = Hearing.query.get_or_404(hearing_id)
 
@@ -40,3 +42,32 @@ def create(hearing_id):
     db.session.commit()
 
     return jsonify({'comments': CommentSchema(comment).data}), 201
+
+
+@comment.route('/comments/<int:comment_id>/links/likes', methods=['POST'])
+def like(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+
+    user_id = request.get_json().get('user_id', 0)
+    user = User.query.get_or_404(user_id)
+
+    if user != current_user:
+        abort(401)
+
+    try:
+        like = (
+            Like.query
+            .filter(User.id == user_id, Comment.id == comment_id)
+            .one()
+        )
+        db.session.delete(like)
+    except NoResultFound:
+        like = Like(
+            user=user,
+            comment=comment
+        )
+        db.session.add(like)
+
+    db.session.commit()
+
+    return jsonify({'like_count': comment.like_count}), 201
