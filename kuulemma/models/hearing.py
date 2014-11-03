29fc14213459b34e95d0f7a9b475412eb717443a
Lauri Raivio <lauri.raivio@gmail.com>
2014-11-03 09:43:55 +0200

    Add published, opens_at and closes_at to hearing

--- kuulemma/models/hearing.py
+++ kuulemma/models/hearing.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import date
+
 from inflection import parameterize
 
 from kuulemma.extensions import db
@@ -11,6 +13,23 @@ class Hearing(db.Model, TextItemMixin):
     __versioned__ = {}
     __tablename__ = 'hearing'
 
+    opens_at = db.Column(
+        db.Date,
+        nullable=True,
+    )
+
+    closes_at = db.Column(
+        db.Date,
+        nullable=True,
+    )
+
+    published = db.Column(
+        db.Boolean(),
+        nullable=False,
+        default=False,
+        server_default='FALSE'
+    )
+
     alternatives = db.relationship(
         HearingSection,
         cascade='all, delete-orphan',
@@ -21,3 +40,10 @@ class Hearing(db.Model, TextItemMixin):
     @property
     def slug(self):
         return parameterize(self.title)
+
+    @property
+    def days_open(self):
+        if self.closes_at:
+            days_open = self.closes_at - date.today()
+            return max(0, days_open.days)
+        return 0
