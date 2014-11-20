5a23904ef475d1f2c298dfea33b95eb763302ee9
Lauri Raivio <lauri.raivio@gmail.com>
2014-11-20 09:31:38 +0200

    Remove unused code

--- kuulemma/views/hearing.py
+++ kuulemma/views/hearing.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, redirect, render_template, url_for
-from sqlalchemy import desc
 
-from ..models import Comment, Hearing
+from ..models import Hearing
 
 hearing = Blueprint(
     name='hearing',
@@ -33,24 +32,11 @@ def show(hearing_id, slug):
             url_for('hearing.show', hearing_id=hearing_id, slug=hearing.slug)
         )
 
-    latest_comments = (
-        Comment.query
-        .filter(Comment.hearing == hearing)
-        .order_by(desc(Comment.created_at))
-    )
-    # TODO: Change this into number of likes when likes are implemented.
-    popular_comments = (
-        Comment.query
-        .filter(Comment.hearing == hearing)
-    )
-
     commentable_sections_string = hearing.get_commentable_sections_string()
 
     return render_template(
         'hearing/show.html',
         hearing=hearing,
-        latest_comments=latest_comments,
-        popular_comments=popular_comments,
         commentable_sections_string=commentable_sections_string,
         hearing_page_active=True
     )
