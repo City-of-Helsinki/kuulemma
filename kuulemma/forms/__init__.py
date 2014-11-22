0868f2baa84adc1814c9acb8cbd64d10a8a9df5e
Samuli Suortti <samuli.suortti@gmail.com>
2014-11-22 17:19:48 +0200

    Add signup form

--- kuulemma/forms/__init__.py
+++ kuulemma/forms/__init__.py
@@ -1,1 +1,20 @@
-from .login import LoginForm  # noqa
+from flask import current_app
+from flask.ext import wtf
+from flask.ext.babel import get_locale
+
+from wtforms.ext.i18n.utils import get_translations
+
+
+class Form(wtf.Form):
+    translations_cache = {}
+    csrf_enabled = False
+
+    def _get_translations(self):
+        locale = get_locale()
+        if not locale:
+            return None
+        fallback_language = current_app.config['BABEL_DEFAULT_LOCALE']
+        languages = (locale.language, fallback_language)
+        if languages not in self.translations_cache:
+            self.translations_cache[languages] = get_translations(languages)
+        return self.translations_cache[languages]
