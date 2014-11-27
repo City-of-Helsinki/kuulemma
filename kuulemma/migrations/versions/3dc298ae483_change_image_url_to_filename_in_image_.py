fa5749050d00c63acb562862b2ea3603e8d52075
Lauri Raivio <lauri.raivio@gmail.com>
2014-11-27 19:03:39 +0200

    Add open graph metadata

--- kuulemma/migrations/versions/3dc298ae483_change_image_url_to_filename_in_image_.py
+++ kuulemma/migrations/versions/3dc298ae483_change_image_url_to_filename_in_image_.py
@@ -10,32 +10,18 @@ from alembic import op
 def upgrade():
     op.alter_column('image', 'image_url', new_column_name='filename')
 
-    conn = op.get_bind()
-    rows = conn.execute('SELECT * FROM image')
-
-    for row in rows:
-        conn.execute('''
-            UPDATE image
-            SET filename = '{filename}'
-            WHERE id = {id}
-        '''.format(
-            filename=row['filename'].replace('/static/', ''),
-            id=row['id']
-        ))
+    op.execute(
+        '''
+        UPDATE image SET filename = regexp_replace(filename, '^/static/', '')
+        '''
+    )
 
 
 def downgrade():
-    conn = op.get_bind()
-    rows = conn.execute('SELECT * FROM image')
-
-    for row in rows:
-        conn.execute('''
-            UPDATE image
-            SET filename = '/static/{filename}'
-            WHERE id = {id}
-        '''.format(
-            filename=row['filename'],
-            id=row['id']
-        ))
+    op.execute(
+        '''
+        UPDATE image SET filename = CONCAT('/static/', filename)
+        '''
+    )
 
     op.alter_column('image', 'filename', new_column_name='image_url')
