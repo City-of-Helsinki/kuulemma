a942a4660cee03a719ff079132d847831cd2af50
Roope Hovi <roope.hovi@gmail.com>
2014-12-03 19:00:59 +0200

    Update UI to support comment editing

--- kuulemma/static/app/components/comment-list-item/comment-body-filter_test.js
+++ kuulemma/static/app/components/comment-list-item/comment-body-filter_test.js
@@ -9,14 +9,14 @@ describe('Filter: commentBodyFilter', function () {
     commentBodyFilter = $filter('commentBodyFilter');
   }));
 
-  it('replace line breaks with <br> tags', function () {
+  it('should replace line breaks with <br> tags', function () {
     var text = 'line1\nline2\n\nline3';
     expect(commentBodyFilter(text)).toBe('line1<br>line2<br><br>line3');
   });
 
   it('should do nothing if no line breaks', function() {
     var text = 'line1';
-    expect(commentBodyFilter(text)).toBe('line1');
+    expect(commentBodyFilter(text)).toBe(text);
   });
 
 });
--- kuulemma/static/app/components/comment-list-item/comment-list-item-directive_test.js
+++ kuulemma/static/app/components/comment-list-item/comment-list-item-directive_test.js
@@ -9,16 +9,18 @@ describe('Directive: commentListItemDirective', function () {
     'kuulemma/static/app/components/comment-adder/comment-adder.html'
   ));
 
-  var element, scope, isolateScope;
+  var element, scope, isolateScope, $httpBackend, CommentListService;
 
-  beforeEach(inject(function ($rootScope, $templateCache, $compile, $httpBackend) {
+  beforeEach(inject(function ($rootScope, $templateCache, $compile, _$httpBackend_, _CommentListService_) {
     var commentListItemTemplate = $templateCache.get('kuulemma/static/app/components/comment-list-item/comment-list-item.html');
     var commentAdderTemplate = $templateCache.get('kuulemma/static/app/components/comment-adder/comment-adder.html');
 
     $templateCache.put('/static/dist/partials/components/comment-list-item/comment-list-item.html', commentListItemTemplate);
     $templateCache.put('/static/dist/partials/components/comment-adder/comment-adder.html', commentAdderTemplate);
     scope = $rootScope.$new();
-    scope.$parent.comment = {body: 'comment', parent_preview: ''};
+    scope.$parent.comment = {body: 'comment', title: 'title', username: 'username', is_hidden: false, parent_preview: '', id: 1};
+    $httpBackend = _$httpBackend_;
+    CommentListService = _CommentListService_;
 
     element = angular.element('<div comment-list-item comment="comment" hearing-id="2"></div>');
     element = $compile(element)(scope);
@@ -28,5 +30,61 @@ describe('Directive: commentListItemDirective', function () {
 
   it('should pass hearing id to controller', function () {
     expect(isolateScope.hearingId).toBe('2');
+  });
+
+  describe('Editing comments', function() {
+    var requestHandler;
+    beforeEach(function() {
+      spyOn(CommentListService, 'editComment').andCallThrough();
+    });
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    describe('Saving comments successfully', function() {
+      beforeEach(function() {
+        $httpBackend.expectPUT('/hearings/2/links/comments/1', {
+          title: 'title',
+          body: 'comment',
+          username: 'username',
+          is_hidden: false
+        }).respond(201);
+        isolateScope.saveChanges();
+        $httpBackend.flush();
+      });
+
+      it('should make a put requests via CommentListService', function() {
+        expect(CommentListService.editComment.callCount).toBe(1);
+        expect(CommentListService.editComment).toHaveBeenCalledWith({
+          hearingId: '2',
+          comment: {
+            body: 'comment',
+            title: 'title',
+            username: 'username',
+            is_hidden: false,
+            parent_preview: '',
+            id: 1
+          }
+        });
+      });
+    });
+
+    describe('Saving comments failing', function() {
+      beforeEach(function() {
+        $httpBackend.expectPUT('/hearings/2/links/comments/1', {title: 'title',
+          body: 'edited comment',
+          username: 'username',
+          is_hidden: false
+        }).respond(400);
+        scope.$parent.comment.body = 'edited comment';
+        isolateScope.saveChanges();
+        $httpBackend.flush();
+      });
+      it('should revert changes if saving fails', function() {
+        expect(scope.$parent.comment.body).toBe('comment');
+      });
+    });
   });
 });
