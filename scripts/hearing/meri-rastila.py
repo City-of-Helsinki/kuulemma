b673ac17e77fb3ab2bd3cd3ed8929fc5198bc878
Rikuoja <riku.oja@kapsi.fi>
2015-11-25 18:54:26 +0200

    Add meri-rastila hearing

---
+++ scripts/hearing/meri-rastila.py
@@ -0,0 +1,586 @@
+# -*- coding: utf-8 -*-
+# Kuulemma
+# Copyright (C) 2014, Fast Monkeys Oy
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import date
+
+
+# Main section
+# ------------
+
+title = 'Meri-Rastilan kaupunkiuudistus'
+
+slug = 'meri-rastila'
+
+main_image = {
+    'filename': 'images/{slug}/main_image.jpg'.format(slug=slug),
+    'caption': (
+        'Yleiskuva. '
+        '(Kaupunkisuunnitteluvirasto / Linda Wiksten)'
+    )
+}
+
+lead = '''
+
+    Meri-Rastilan nykyiselle asuinalueelle suunnitellaan uutta asuinrakentamista.
+    Alueelle voisi valmistua 10–15 vuoden kuluessa asuntoja noin 3 300–4 600
+    uudelle asukkaalle. Tämä mahdollistaisi alueen puistojen, aukioiden, katujen
+    ja metroaseman ympäristön parantamistoimet. Lisärakentamisella pyritään myös
+    turvaamaan palveluiden säilyminen alueella.
+
+'''
+
+body = '''
+
+    <p>
+    Meri-Rastilan kehittämisestä on tuotu keskusteluun alustava suunnitelma,
+    johon nyt toivotaan asukkaiden ja alueen toimijoiden näkemyksiä. Suunnitelma
+    sisältää sekä täydennysrakentamissuunnitelmia alueittain että viheralueiden
+    ja kaupunkitilojen kehittämissuunnitelman. Suunnittelutyö on vielä
+    alkuvaiheessa. Siksi esitetyissä kuvissa on keskenään erilaisia ratkaisuja.
+    Nyt on hyvä mahdollisuus keskustella ratkaisuista, esim. rakentamisen ja
+    puistotoimintojen sijoittelusta. Suunnitelmia voi kommentoida alla
+    teemoittain ja alueittain 30. lokakuuta asti.
+    </p>
+    <p>
+    Kerro erityisesti
+    </p>
+    <ul>
+    <li>
+    mitä tärkeää toimintaa kullakin virkistysalueella tai puistossa on ja miten
+    kyseistä aluetta pitäisi kehittää
+    </li>
+    <li>
+    mikä täydennysrakentamisen paikka on hyvä tai mikä huono – ja miksi
+    </li>
+    <li>
+    mikä alueista pitäisi toteuttaa ensimmäisessä vaiheessa tai mikä viimeiseksi
+    – miksi?
+    </li>
+    </ul>
+    <p>
+    Alustavan suunnitelman ja saadun palautteen pohjalta valmistellaan
+    asemakaavaluonnos, joka on tarkoitus laittaa nähtäville vuodenvaihteessa
+    2015–2016 ja viedä tämän jälkeen kaupunkisuunnittelulautakunnan käsittelyyn.
+    </p>
+    <h4>Linkit</h4>
+    <ul>
+      <li><a
+      href="http://kartta.hel.fi/applications/hanke/showplan.aspx?map=yes&ID=2015-002625"
+      target="_blank">Hankkeen suunnitteluaineistot</a></li>
+      <li><a
+      href="http://helsinki.creamailer.fi/subscribe/54d4cbd49c42c"
+      target="_blank">Tilaa uutiskirje sähköpostiisi</a></li>
+      <li><a href="https://www.facebook.com/helsinkisuunnittelee/events">
+      Kävelykierros ja keskustelu alueella 5. ja 7. lokakuuta klo 17</a>. Tervetuloa!</li>
+    </ul>
+
+'''
+
+opens_at = date(2015, 10, 1)
+
+closes_at = date(2015, 10, 30)
+
+published = True
+
+area = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [
+                25.13390779495239,
+                60.205710352530346
+            ],
+            [
+                25.13444423675537,
+                60.20419630670974
+            ],
+            [
+                25.135023593902588,
+                60.2039937178139
+            ],
+            [
+                25.134294033050537,
+                60.20382311566814
+            ],
+            [
+                25.133607387542725,
+                60.20246892966917
+            ],
+            [
+                25.134100914001465,
+                60.20197842331377
+            ],
+            [
+                25.135023593902588,
+                60.2012426500314
+            ],
+            [
+                25.13249158859253,
+                60.20072013434833
+            ],
+            [
+                25.132277011871338,
+                60.19976038998808
+            ],
+            [
+                25.13047456741333,
+                60.1978088232189
+            ],
+            [
+                25.128285884857178,
+                60.19802211485371
+            ],
+            [
+                25.127921104431152,
+                60.197830152444766
+            ],
+            [
+                25.125024318695065,
+                60.19921652239
+            ],
+            [
+                25.120904445648193,
+                60.19769151222884
+            ],
+            [
+                25.119144916534424,
+                60.197040961854974
+            ],
+            [
+                25.118544101715088,
+                60.19745688899067
+            ],
+            [
+                25.119831562042236,
+                60.19828872744773
+            ],
+            [
+                25.11886596679687,
+                60.19867264577775
+            ],
+            [
+                25.119273662567135,
+                60.198917924581586
+            ],
+            [
+                25.117342472076416,
+                60.19964308597447
+            ],
+            [
+                25.115883350372314,
+                60.19968574202793
+            ],
+            [
+                25.114595890045166,
+                60.200154954956425
+            ],
+            [
+                25.11603355407715,
+                60.20149857304445
+            ],
+            [
+                25.116076469421387,
+                60.202436940347766
+            ],
+            [
+                25.115840435028076,
+                60.20320467545322
+            ],
+            [
+                25.11652708053589,
+                60.20404703080252
+            ],
+            [
+                25.11601209640503,
+                60.20445220668483
+            ],
+            [
+                25.11697769165039,
+                60.20562505603163
+            ],
+            [
+                25.118930339813232,
+                60.20521989463408
+            ],
+            [
+                25.11953115463257,
+                60.20582763485396
+            ],
+            [
+                25.120625495910645,
+                60.20561439395369
+            ],
+            [
+                25.120131969451904,
+                60.204995987504525
+            ],
+            [
+                25.122900009155273,
+                60.20461214315566
+            ],
+            [
+                25.124895572662354,
+                60.2044415442257
+            ],
+            [
+                25.12695550918579,
+                60.204430881763116
+            ],
+            [
+                25.1302170753479,
+                60.204836052904945
+            ],
+            [
+                25.13390779495239,
+                60.205710352530346
+            ]
+        ]
+    ]
+}
+
+# section 1
+# -------------
+
+section_1_title = 'Virkistysalue- ja puistosuunnitelmat'
+
+section_1_main_image = {
+    'filename': 'images/{slug}/section_1/main_image.jpg'.format(slug=slug),
+    'caption': 'Osa-alueen esimerkkikuva.'
+}
+
+section_1_lead = '''
+
+    Meri-Rastilan viheralueiden ja kaupunkitilojen kehittämisen
+    ideasuunnitelma tähtää alueen puistojen, aukioiden, katujen ja raittien
+    parantamiseen. Lähtökohtana on kehittää Meri-Rastilan keskeisiä
+    vetovoimatekijöitä: vehreitä kävelyreittejä, arvokkaita luontoarvoja
+    sekä yhteisöllistä puistoa Haruspuistossa.
+
+'''
+
+section_1_body = '''
+
+    <p>
+    Liikunta- ja leikkitoiminnot sijoittuvat Ison Kallahden liikuntapuistoon
+    ja Haruspuistoon. Ison Kallahden puistossa liikuntatoiminnot sijoitetaan
+    niin, että ne kunnioittavat arvokasta ranta-aluetta ja rantahuviloiden
+    aluetta. Suunnitelmassa on esitetty kaksi eri versiota liikuntapuiston
+    toteutukselle. Toisessa on runsaammin erilaisia liikuntakenttiä ja
+    toisessa tilaa on jätetty monitoiminurmelle.
+    </p>
+    <p>
+    Haruspuiston asemaa alueen tärkeänä monikulttuurisena asukaspuistona
+    vahvistetaan. Leikkipuiston toimintoja kehitetään ja nuorille luodaan
+    uusia toiminnallisia oleskelupaikkoja. Haruspuiston avointa maisemaa
+    lisätään nurmialuein.
+    </p>
+    <p>
+    Pohjanveden puisto ja Ole Kandelinin puisto säilyvät luontoarvoiltaan ja
+    ilmeiltään rikkaina lähimetsinä. Puistojen reittejä ja valaistusta
+    parannetaan. Pohjavedenpuistossa nostetaan esiin muutamia merkittäviä
+    kohtia, kuten näköalapaikkoja tai reittien varrella olevia uusia leikki-
+    tai liikuntapaikkoja.
+    </p>
+    <p>
+    Rysäpuisto on tärkeä kävelyn ja pyöräilyn reittien risteämiskohta, jota
+    voidaan korostaa penkein ja pienimuotoisen toiminnallisen aktiviteetin
+    avulla.
+    </p>
+    <p>
+    Katuympäristöä kehitetään täydennysrakentamisen yhteydessä. Etenkin
+    katualueella risteäviä jalankulku- ja pyöräväyliä tulee korostaa
+    katuympäristöstä, niin että suunnistaminen ja tunnistaminen alueella
+    helpottuvat, ja että kadun ylitys sujuu mahdollisimman luontevasti.
+    </p>
+    <p>
+    Lähiöprojekti jatkaa Meri-Rastilan kaupunkitilojen kehittämistyötä
+    tarkentamalla puistojen ja muiden julkisten ulkotilojen suunnittelua
+    asemakaavahankkeen edetessä. Tavoitteena on käynnistää kaupunkitilojen
+    seuraava suunnitteluvaihe syksyllä 2016.
+    </p>
+    <a href="http://lahioprojekti.hel.fi/sites/default/files/files/Julkisentilansuunnitelmat/meri-rastila_kaupunkitilojen_ideasuunnitelma_netti3s.pdf">
+    Tutustu ideasuunnitelmaan</a>.
+    </p>
+    <h4>
+    Kommentoi kaupunkitilojen ideasuunnitelmaa jatkosuunnittelua varten.
+    </h4>
+    <ul>
+        <li>
+        Mitkä uudet toiminnot ovat tärkeimpiä?
+        </li>
+        <li>
+        Onko ideasuunnitelmassa esitetty toimintojen sijoittelu hyvä?
+        </li>
+        <li>
+        Missä järjestyksessä puistoja ja reittejä tulisi kehittää?
+        </li>
+        <li>
+        Kohti tarkempaa suunnitelmaa: konkreettisia ajatuksia
+        ideasuunnitelman toteutukseen liittyen?
+        </li>
+        <li>
+        Puuttuuko ideasuunnitelmasta jokin olennainen toiminto tai reitti
+        tms.?
+        </li>
+    </ul>
+
+'''
+
+# section 2
+# -------------
+
+section_2_title = 'Lisärakentaminen nykyisillä tonteilla, metroaseman' \
+                  ' lähistöllä sekä Rysäpuiston ja Ole Kandelinin puiston' \
+                  ' laidoilla'
+
+section_2_main_image = {
+    'filename': 'images/{slug}/section_2/main_image.jpg'.format(slug=slug),
+    'caption': 'Osa-alueen esimerkkikuva.'
+}
+
+section_2_lead = '''
+
+    Lisärakentamista on mahdollista saada myös nykyisille asuintonteille.
+    Keskeinen osa Meri-Rastilan kaupunkiuudistusta onkin selvittää
+    taloyhtiöiden tarpeet ja toiveet tonttinsa mahdollisesta
+    lisärakentamisesta. Lisäksi kaupunki tutkii Meri-Rastilan
+    liikekeskuksen kehittämisen mahdollisuuksia ja julkisia palveluja.
+
+'''
+
+section_2_body = '''
+
+    <p>
+    Tonttikohtainen lisärakentaminen voi tarkoittaa esimerkiksi nykyisen
+    rakennuksen korottamista tai jatkamista – tai uuden rakennuksen
+    lisäämistä tontille. Kaupunki maksaa taloyhtiöille lisärakentamisesta
+    korvausta, joilla taloyhtiö voi rahoittaa mm. remontteja. Tontille
+    rakennetaan lisää vain, jos taloyhtiö niin haluaa.
+    </p>
+    <p>
+    Kuvassa on esimerkkejä siitä, miten Meri-Rastilassa on mahdollista
+    sijoittaa uutta rakentamista olemassa oleville tonteille,
+    vierekkäisille tonteille tai tonttien laajentuessa puistoihin tai
+    muille yleisille alueille.
+    </p>
+    <p>
+    Kuvassa esitetty lisärakentaminen on n. 80 000 k-m2, mikä tarkoittaa
+    n. 1 400 uutta asukasta.
+    </p>
+    <p>
+    <a href="http://www.uuttahelsinkia.fi/fi/taydennysrakentaminen">
+    Lisätietoja tonttikohtaisesta lisärakentamisesta</a>
+    </p>
+
+'''
+
+# section 3
+# -------------
+
+section_3_title = 'Haruspuiston alue'
+
+section_3_main_image = {
+    'filename': 'images/{slug}/section_3/main_image.jpg'.format(slug=slug),
+    'caption': 'Osa-alueen esimerkkikuva.'
+}
+
+section_3_lead = '''
+
+    Haruspuiston alueen täydennysrakentamisen vaihtoehtoina ovat:
+
+'''
+
+section_3_body = '''
+
+    <h4>
+    Versio A
+    </h4>
+    <p>
+    Uusi asuinrakentaminen muodostaa kallioiden reunaa täydentävän
+    pienimittakaavaisen (2-4 kerrosta) rakenteen Haruspuiston eteläosaan,
+    joka kasvaa puiston länsi- ja itäosaa kohti (4-6 kerrosta). Osa
+    rakentamisesta sijoittuu Prammikujan päähän puiston lounaisosaan.
+    Korkeaa rakentamista (10 kerrosta) sijoitetaan rantaan johtavan reitin
+    varrelle korostamaan Meri-Rastilan merellisyyttä. Matala rakentaminen
+    sijoitetaan niin, että Haruspuistoon saadaan mahdollisimman paljon valoa.
+    </p>
+
+    <h4>
+    Versio B
+    </h4>
+    <p>
+    Haruspuiston länsiosa säilyy avoimena puistoalueena. Eteläosaan
+    kallioiden päälle sijoitetaan poikittain keskikorkeita kerrostaloja (6
+    kerrosta). Pohjoisosaan nykyisen päiväkodin ja leikkipuistotalon väliin
+    tulee pistemäisiä asuinkerrostaloja (5 kerrosta), jotka yhdessä
+    eteläosan rakennusten kanssa muodostavat kiinnostavan, mutkittelevan
+    puistotilan. Rakennukset on suunnattu siten, että näkymät pihoilta ja
+    asunnoista ovat pitkiä ja valo pääsee Haruspuistoon mahdollisimman hyvin.
+    </p>
+    <p>
+    Kuvissa esitetty täydennysrakentaminen on n. 25 000 k-m2, mikä tarkoittaa
+    n. 500:aa uutta asukasta.
+    </p>
+
+'''
+
+# section 4
+# -------------
+
+section_4_title = 'Pohjavedenpuiston alue'
+
+section_4_main_image = {
+    'filename': 'images/{slug}/section_4/main_image.jpg'.format(slug=slug),
+    'caption': 'Osa-alueen esimerkkikuva.'
+}
+
+section_4_lead = '''
+
+    Pohjavedenpuiston alueen täydennysrakentamisen vaihtoehtoina ovat:
+
+'''
+
+section_4_body = '''
+
+    <h4>
+    Versio A
+    </h4>
+    <p>
+    Kallioalueen koillisosa, kaakkoisosa ja luoteisrinne säilyvät
+    virkistysalueena. Etelä- ja pohjoisosassa säilyvät hienot näköalapaikat.
+    Laaksoalueet rakennetaan pienimittakaavaisesti (2–4 kerrosta), katujen
+    varsilla rakentaminen on hieman korkeampaa (n. 6 kerrosta) ja kallion
+    laella on pistemäisiä korkeita kerrostaloja merinäköaloin (n. 8–10
+    kerrosta). Meri-Rastilan tien varressa on toiminnallinen puiston osa,
+    josta pääsee suoraan luonnonmukaiseen kalliopuistoon.
+    </p>
+
+    <h4>
+    Versio B
+    </h4>
+    <p>
+    Vuotien varteen sijoittuu tiivis korttelirakenne suojaamaan aluetta
+    melulta (3–8 kerrosta). Alueen uudet katuyhteydet ovat
+    etelä-pohjoissuuntaisia ja kytkeytyvät Meri-Rastilan tiehen. Läntisemmän
+    kadun varressa puiston puoleinen rakentaminen on pienimittakaavaista ja
+    pistemäistä (2–3 kerrosta), jolloin rakennusten väleistä saadaan näkymiä
+    puistoon. Puiston itäosan laaksoalueelle sijoittuu keskikorkeaa (6
+    kerrosta) kerrostaloasumista. Eteläosa kallion lakialueesta säästetään
+    virkistysalueena. Meri-Rastilan tie rajautuu asuintaloihin, jolloin se
+    muuttuu ulkoasultaan katumaisemmaksi.
+    </p>
+    <p>
+    Kuvissa esitetty täydennysrakentaminen on n. 50 000 k-m2, mikä tarkoittaa
+    n. 1 000:ta uutta asukasta.
+    </p>
+
+'''
+
+# section 5
+# -------------
+
+section_5_title = 'Ison Kallahdenpuiston alue'
+
+section_5_main_image = {
+    'filename': 'images/{slug}/section_5/main_image.jpg'.format(slug=slug),
+    'caption': 'Osa-alueen esimerkkikuva.'
+}
+
+section_5_lead = '''
+
+    Ison Kallahden puiston alueen täydennysrakentamisen vaihtoehtoina ovat:
+
+'''
+
+section_5_body = '''
+
+    <h4>
+    Versio A
+    </h4>
+    <p>
+    Kallion laella on keskikorkeaa (4–8 kerrosta) kerrostalorakentamista,
+    joka madaltuu ja porrastuu (2–3 kerrrosta) rinnettä alaspäin mentäessä.
+    Kallioalueen eteläosa säilytetään virkistysalueena, jolloin sen laki
+    toimii näköalapaikkana. Harbonkadun varteen tulee pienimittakaavaista
+    (2–3 kerrosta) rivitalorakentamista, joka täydentää olemassa olevan
+    kaupunkirakenteen.
+    </p>
+
+    <h4>
+    Versio B
+    </h4>
+    <p>
+    Harbonkadun varren tasainen niittyalue jätetään rakentamatta, jolloin
+    siihen on mahdollista sijoittaa esimerkiksi tilaa vieviä
+    liikuntatoimintoja kuten pelikenttiä. Kallioalueen päälle sijoittuu
+    korkeaa maamerkkimäistä rakentamista merinäköaloin (8–12 kerrosta).
+    </p>
+    <p>
+    Kuvissa esitetty täydennysrakentaminen on n. 30 000 k-m2, mikä
+    tarkoittaa n. 600:aa uutta asukasta.
+    </p>
+
+'''
+
+
+# Hearing
+# -------
+
+hearing = {
+    'slug': slug,
+    'title': title,
+    'main_image': main_image,
+    'lead': lead,
+    'body': body,
+    'opens_at': opens_at,
+    'closes_at': closes_at,
+    'published': published,
+    'area': area,
+    'sections': [
+        {
+            'title': section_1_title,
+            'main_image': section_1_main_image,
+            'lead': section_1_lead,
+            'body': section_1_body,
+        },
+        {
+            'title': section_2_title,
+            'main_image': section_2_main_image,
+            'lead': section_2_lead,
+            'body': section_2_body,
+        },
+        {
+            'title': section_3_title,
+            'main_image': section_3_main_image,
+            'lead': section_3_lead,
+            'body': section_3_body,
+        },
+        {
+            'title': section_4_title,
+            'main_image': section_4_main_image,
+            'lead': section_4_lead,
+            'body': section_4_body,
+        },
+        {
+            'title': section_5_title,
+            'main_image': section_5_main_image,
+            'lead': section_5_lead,
+            'body': section_5_body,
+        }
+    ],
+    'alternatives': []
+}
